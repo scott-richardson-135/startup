cd55e643b028ca712c4ff99080e72cc5ebdc3c3f
Scott Richardson <srich1550@gmail.com>
2025-02-06 20:53:01 -0700

    getting ready for react

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return <div className='body bg-white'>App will display here</div>;
+}
