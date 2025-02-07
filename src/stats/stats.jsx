dd4dbc97ac008f09c56911f0e5a82f8b7d08b31c
Scott Richardson <srich1550@gmail.com>
2025-02-06 21:04:10 -0700

    added view components

---
+++ src/stats/stats.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Stats() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>Stats displayed here</div>
+    </main>
+  );
+}
