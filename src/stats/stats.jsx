4502c829442d3b93f35b6e2354500051c42201da
Scott Richardson <srich1550@gmail.com>
2025-02-26 15:14:12 -0700

    stats page displays stats from local storage

--- src/stats/stats.jsx
+++ src/stats/stats.jsx
@@ -2,24 +2,29 @@ import React from 'react';
 import './stats.css';
 
 export function Stats() {
+    const stats = JSON.parse(localStorage.getItem("hangleStats")) || { wins: 0, losses: 0, gamesPlayed: 0 };
+    const gamesPlayed = stats.gamesPlayed;
+    const wins = stats.wins;
+    const losses = stats.losses;
+
   return (
     <main className='container-fluid bg-white text-center'>
         <h2>Player Statistics</h2>
 
         <div className="stat">
-            <p>Total games played: <span id="total_games">placeholder</span></p>
+            <p>Total games played: <span id="total_games">{gamesPlayed}</span></p>
         </div>
 
         <div className="stat">
-            <p>Games won: <span id="wins" className="text-success">placeholder</span></p>
+            <p>Games won: <span id="wins" className="text-success">{wins}</span></p>
         </div>
 
         <div className="stat">
-            <p>Games lost: <span id="losses" className="text-danger">placeholder</span></p>
+            <p>Games lost: <span id="losses" className="text-danger">{losses}</span></p>
         </div>
 
         <div className="stat">
-            <p>Win percentage: <span id="winpct">placeholder</span></p>
+            <p>Win percentage: <span id="winpct">%{(wins / gamesPlayed) * 100}</span></p>
         </div>
     </main>
   );
