95830f5e1d6b94581c488224aeb29ea2002726e0
Scott Richardson <srich1550@gmail.com>
2025-03-11 11:17:25 -0600

    added api router

--- service/index.js
+++ service/index.js
@@ -4,10 +4,21 @@ const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 const app = express();
 
+//data structures
+let users = [];
+let stats = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use(express.json());
+
 app.use(express.static('public'));
+
+
+//api path for endpoints
+let apiRouter = express.Router();
+app.use('/api', apiRouter);
+
 
 app.get('*', (_req, res) => {
     res.send({ msg: 'Startup service' });
