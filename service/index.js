64139988a5e223a8543fcdc858e1e1f24b44cabd
Scott Richardson <srich1550@gmail.com>
2025-03-11 10:56:39 -0600

    npm packages

--- service/index.js
+++ service/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
 const app = express();
+
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
