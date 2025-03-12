cd6980c210b1eea6e56b67bcd05a27db5d1face6
Scott Richardson <srich1550@gmail.com>
2025-03-11 21:11:12 -0600

    logout endpoint

--- service/index.js
+++ service/index.js
@@ -47,6 +47,16 @@ apiRouter.post('/auth/login', async (req, res) => {
     }
 });
 
+//logout
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
 
 
 
